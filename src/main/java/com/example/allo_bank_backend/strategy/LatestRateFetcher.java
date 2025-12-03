3feabfd3a21364fb0004ee8fe3563578afd1a613
Ahmad Romiz Ilhamy <aromiz@jakartamrt.co.id>
2025-12-03 15:46:42 +0700

    feat : implemented latest rate strategy

---
+++ src/main/java/com/example/allo_bank_backend/strategy/LatestRateFetcher.java
@@ -0,0 +1,48 @@
+package com.example.allo_bank_backend.strategy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+@Component
+public class LatestRateFetcher implements IDRDataFetcher {
+    private final WebClient client;
+
+    @Value("${internal.spread-factor}")
+    private String spreadFactor;
+
+    public LatestRateFetcher(WebClient client) {
+        this.client = client;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Object fetch() {
+
+        Map<String, Object> data = client.get()
+                .uri("/latest?base=IDR")
+                .retrieve()
+                .bodyToMono(Map.class)
+                .block();
+
+        if (data == null || data.get("rates") == null) {
+            throw new IllegalStateException("Missing 'rates' field in response");
+        }
+
+        Map<String, Double> rates = (Map<String, Double>) data.get("rates");
+        double usdRate = rates.get("USD");
+
+        double spread = (1 / usdRate) * (1 + Double.parseDouble(spreadFactor));
+
+        data.put("USD_BuySpread_IDR", spread);
+
+        return data;
+    }
+
+    @Override
+    public String getType() {
+        return "latest_idr_rates";
+    }
+}
