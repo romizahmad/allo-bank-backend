852f2ce3b8d7bef52a39c75c27cd94be4b1a26e8
Ahmad Romiz Ilhamy <aromiz@jakartamrt.co.id>
2025-12-03 15:16:12 +0700

    first commit

---
+++ src/main/java/com/example/allo_bank_backend/AlloBankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.allo_bank_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlloBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlloBankBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/allo_bank_backend/AlloBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.allo_bank_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlloBankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
